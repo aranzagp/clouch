4fb45bf62d8c8407fb4647263a01162cf2b14ad5
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2015-03-19 13:55:05 -0600

    Add tag type to manage struct tags - Move tests structs to another file to slim tests file

---
+++ mapify/tests_structs_test.go
@@ -0,0 +1,39 @@
+package mapify_test
+
+type TestA struct {
+	ID    string
+	Revs  []string
+	Hello string
+}
+
+type TestB struct {
+	ID    string
+	Revs  []string
+	Hello string
+	Foo   *string
+	Num   *int
+}
+
+type TestC struct {
+	ID    string
+	Revs  []string
+	TestA TestA
+}
+
+type TestD struct {
+	ID    string
+	Revs  []string
+	TestA *TestA
+}
+
+type TestE struct {
+	ID    string
+	Revs  []string
+	TestD TestD
+}
+
+type TestF struct {
+	DocID    string   `clouch:"_id"`
+	Revision []string `clouch:"_revs"`
+	Hello    string   `clouch:"hello"`
+}
